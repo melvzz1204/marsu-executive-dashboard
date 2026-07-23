811daadda4a44f0c31d3f98052cf5a194297bef1
melvzz1204 <137977788+melvzz1204@users.noreply.github.com>
2026-07-23 15:10:06 +0800

    new update

--- frontend/src/components/EnrollmentChart.jsx
+++ frontend/src/components/EnrollmentChart.jsx
@@ -1,5 +1,5 @@
 /* import UndergradEnrollment from "../staticData/enrollments/undergradEnrollmentData.jsx";
- */ import EnrollmentFull from "../staticData/enrollments/enrollmentFull.jsx";
+ */ import EnrollmentFull from "../components/Admin/Dashboards/enrollmentDashboard";
 
 export default function EnrollmentChart() {
   return (
