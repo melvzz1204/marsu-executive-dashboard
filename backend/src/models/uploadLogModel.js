3b528dcb3baea95fbf83358f9a660862d419b249
Marc Malacas <malacasmarc16@gmail.com>
2026-08-05 15:03:44 +0800

    Patch the upload log model

--- backend/src/models/uploadLogModel.js
+++ backend/src/models/uploadLogModel.js
@@ -4,7 +4,7 @@ const uploadLogSchema = new mongoose.Schema(
   {
     module: {
       type: String,
-      enum: ["ENROLLMENT", "HIGHER_EDUCATION", "RESEARCH", "RESEARCH_EXTENSION", "SYSTEM"],
+      enum: ["ENROLLMENT", "HIGHER_EDUCATION", "RESEARCH_EXTENSION", "SYSTEM"],
       default: "ENROLLMENT",
       trim: true,
     },
@@ -23,7 +23,13 @@ const uploadLogSchema = new mongoose.Schema(
     },
     status: {
       type: String,
-      enum: ["SUCCESS", "FAILED", "DUPLICATE_BLOCK", "PARTIAL_SUCCESS"],
+      enum: [
+        "SUCCESS",
+        "FAILED",
+        "DUPLICATE_BLOCK",
+        "PARTIAL_SUCCESS",
+        "OVERWRITE",
+      ],
       required: true,
     },
     groupsProcessed: {
@@ -57,7 +63,7 @@ const uploadLogSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 module.exports = mongoose.model("UploadLog", uploadLogSchema);
